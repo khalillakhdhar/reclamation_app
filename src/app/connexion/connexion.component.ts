e1329eacc78317412ffe0651607c38aa8cc620ad
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-04-05 11:09:10 +0100

    Update connexion.component.ts

--- src/app/connexion/connexion.component.ts
+++ src/app/connexion/connexion.component.ts
@@ -9,15 +9,60 @@ import { UserService } from '../services/user.service';
 })
 export class ConnexionComponent implements OnInit {
   user:Utilisateur;
+  users:Utilisateur[];
+  etat=false;
   constructor( private userService:UserService) { }
 
   ngOnInit(): void {
     this.user=new Utilisateur();
+    this.read();
+  }
+  read()
+  {
+    this.userService.read_Users().subscribe(data => {
+
+      this.users = data.map(e => {
+        return {
+         id: e.payload.doc.id,
+
+         nom: e.payload.doc.data()["nom"],
+         prenom: e.payload.doc.data()["prenom"],
+         login: e.payload.doc.data()["login"],
+         mdp: e.payload.doc.data()["mdp"],
+         grade: e.payload.doc.data()["grade"],
+         zone: e.payload.doc.data()["zone"],
+         cin: e.payload.doc.data()["cin"],
+
+
+
+        };
+      });
+      console.log(this.users);
+
+    });
+
+
   }
   connexion()
   {
+    for(let us of this.users)
+    {
+      if((us.login==this.user.login)&&(us.mdp==this.user.mdp))
+      {
+        this.etat=true;
+        localStorage.setItem("id",us.id);
+        localStorage.setItem("grade",us.grade);
+        window.location.replace("");
+      }
 
-    
+
+
+    }
+    if(this.etat==false)
+    alert("compte inconnu!");
+
+
+
   }
 
 }
