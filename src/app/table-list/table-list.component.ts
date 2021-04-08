f3023e3449660293220f95f29f89aa7e6b5e6df4
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-04-08 10:40:40 +0100

    Update table-list.component.ts

--- src/app/table-list/table-list.component.ts
+++ src/app/table-list/table-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Utilisateur } from '../classes/utilisateur';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-table-list',
@@ -6,10 +8,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table-list.component.css']
 })
 export class TableListComponent implements OnInit {
+  user:Utilisateur;
+  users:Utilisateur[];
+    constructor(private userService:UserService) { }
+  
+    ngOnInit(): void {
+      this.user=new Utilisateur();
+      this.read();
+    }
 
-  constructor() { }
+  read()
+  {
+  this.userService.read_Users().subscribe(data => {
+  
+    this.users = data.map(e => {
+      return {
+        id: e.payload.doc.id,
 
-  ngOnInit() {
+        nom: e.payload.doc.data()["nom"],
+        prenom: e.payload.doc.data()["prenom"],
+        login: e.payload.doc.data()["login"],
+        mdp: e.payload.doc.data()["mdp"],
+        grade: e.payload.doc.data()["grade"],
+        zone: e.payload.doc.data()["zone"],
+        cin: e.payload.doc.data()["cin"],
+
+  
+      };
+    });
+  
+  
+    console.log("liste",this.users);
+  
+  });
+  
+  
+  
+  }
+  supprimer(id)
+  {
+    if(confirm("vous voulez supprimer l'utilisateur?"))
+    this.userService.delete_User(id);
   }
 
 }
