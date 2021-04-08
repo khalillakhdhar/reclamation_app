2002cf3e53d277007d69d302bd5175734044bf83
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-04-08 10:39:42 +0100

    Update user-profile.component.ts

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Utilisateur } from '../classes/utilisateur';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,9 +9,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  id:string;
+  grade:string;
+  
+  user:Utilisateur;
+  users:Utilisateur[];
+  constructor(private userService:UserService) { }
+  
+  ngOnInit(): void {
+    this.user=new Utilisateur();
+    this.id=localStorage.getItem("id");
+    this.read();
+  }
+  read()
+  {
+  this.userService.read_Users().subscribe(data => {
+  
+    this.users = data.map(e => {
+      return {
+       id: e.payload.doc.id,
+  
+       nom: e.payload.doc.data()["nom"],
+       prenom: e.payload.doc.data()["prenom"],
+       cin: e.payload.doc.data()["cin"],
+       zone: e.payload.doc.data()["zone"],
+       login: e.payload.doc.data()["login"],
+       mdp: e.payload.doc.data()["mdp"],
+       grade: e.payload.doc.data()["grade"],
+  
+  
+  
+      };
+    });
+    for (let u of this.users)
+  {
+  if((u.id==this.id))
+  {
+  this.user=u;
+  
+  }
+  console.log("user",this.user);
+  
+  
+  }
+  
+    console.log("liste",this.users);
+  
+  });
+  
+  
+  
+  }
+  update()
+  {
+    let us=Object.assign({},this.user);
+  this.userService.update_User(this.id,us);
+  alert("updated successfully");
+  
+  
   }
 
 }
